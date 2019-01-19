49f04581af48ed955a06f26fb6d6777deea4e5bd
Willane Paiva <willanepaiva@hotmail.com>
2019-01-19 15:04:58 -0300

    Add files for construction

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-import Hello from './Hello';
+import { View } from 'react-native';
 import styles from './Styles';
 
 class App extends Component{
   constructor(){
     super();
-    this.state = {name:'João'}
+    this.state = {
+      todos:[]
+    }
   }
   render() {
-    const result = 1;
+
     return (
       <View style={styles.container}>
-        <Hello name="Maira" />
-        <Text style={styles.welcome}>{result}</Text>
+       
       </View>
     );
   }
---
+++ components/todo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class componentName extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+}
+
+
+export default componentName
