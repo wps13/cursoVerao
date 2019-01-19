df8cb6dbe06a17cb0dc9887c043b6ae4388c6c74
Willane Paiva <willanepaiva@hotmail.com>
2019-01-19 14:46:39 -0300

    Create files for app styles

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import Hello from './Hello';
+import styles from './Styles';
 
 class App extends Component{
   constructor(){
@@ -18,23 +19,5 @@ class App extends Component{
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 export default App;
